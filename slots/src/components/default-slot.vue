1c0c326995c5e652a1b46dea87779484c058c3a6
Waswa Olunga <32232146+iamwaswa@users.noreply.github.com>
2019-12-05 19:57:56 -0800

    Add slots content
    Description of scoped slots

---
+++ slots/src/components/default-slot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot v-bind:user="user">
+      {{ user.lastName }}
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: `default-slot`,
+  data: function() {
+    return {
+      user: {
+        firstName: `Steve`,
+        lastName: `Jobs`
+      }
+    };
+  }
+});
+</script>
---
+++ slots/src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <DefaultSlot>
+      <template v-slot:default="{ user }">
+        {{ user.firstName }}
+      </template>
+    </DefaultSlot>
+  </div>
+</template>
+
+<script>
+import DefaultSlot from "@/components/default-slot";
+
+export default {
+  name: `home`,
+  components: {
+    DefaultSlot
+  }
+};
+</script>
